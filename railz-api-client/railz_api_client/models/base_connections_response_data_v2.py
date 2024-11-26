0ccd5c81d827e81cef382e3fe8ae4b231bd801a1
Eitan Mosenkis <eitan@levro.com>
2024-11-26 12:58:00 +0200

    Make GetExpenseResponseV2Dto.meta optional.

--- railz-api-client/railz_api_client/models/base_connections_response_data_v2.py
+++ railz-api-client/railz_api_client/models/base_connections_response_data_v2.py
@@ -23,52 +23,45 @@ T = TypeVar("T", bound="BaseConnectionsResponseDataV2")
 class BaseConnectionsResponseDataV2:
     """
     Attributes:
-        connection_uuid (str):  Example: CON-e66400c1-4d2f-4683-9bea-275799943695.
-        service_name (BaseConnectionsResponseDataV2ServiceName):  Example: xero.
-        status (str):  Example: active.
-        created_at (Union[None, datetime.datetime]):  Example: 2020-12-25T01:02:03Z.
-        updated_at (Union[None, datetime.datetime]):  Example: 2020-12-25T01:02:03Z.
+        connection_uuid (Union[None, Unset, str]):  Example: CON-e66400c1-4d2f-4683-9bea-275799943695.
         first_record_date (Union[None, Unset, datetime.datetime]):  Example: 2020-12-25T01:02:03Z.
-        description (Union[Unset, str]):  Example: TD Bank.
-        disconnect_reason (Union[Unset, str]):  Example: disconnectedByCustomer.
-        sync_time (Union[Unset, float]):  Example: 1.3092.
-        institution (Union[Unset, ConnectionsInstitutionData]):
-        is_mock_data (Union[Unset, bool]):  Example: True.
-        service_account_ref (Union[Unset, ServiceAccountRefType]):
+        service_name (Union[Unset, BaseConnectionsResponseDataV2ServiceName]):  Example: xero.
+        description (Union[None, Unset, str]):  Example: TD Bank.
+        status (Union[None, Unset, str]):  Example: active.
+        created_at (Union[None, Unset, datetime.datetime]):  Example: 2020-12-25T01:02:03Z.
+        updated_at (Union[None, Unset, datetime.datetime]):  Example: 2020-12-25T01:02:03Z.
+        disconnect_reason (Union[None, Unset, str]):  Example: disconnectedByCustomer.
+        sync_time (Union[None, Unset, float]):  Example: 1.3092.
+        institution (Union['ConnectionsInstitutionData', None, Unset]):  Example: {'name': 'Citizens bank', 'logo':
+            'https://serverlogo.url'}.
+        is_mock_data (Union[None, Unset, bool]):  Example: True.
+        service_account_ref (Union['ServiceAccountRefType', None, Unset]):  Example: {'id': '12345', 'entityRef': {'id':
+            '100'}}.
     """
 
-    connection_uuid: str
-    service_name: BaseConnectionsResponseDataV2ServiceName
-    status: str
-    created_at: Union[None, datetime.datetime]
-    updated_at: Union[None, datetime.datetime]
+    connection_uuid: Union[None, Unset, str] = UNSET
     first_record_date: Union[None, Unset, datetime.datetime] = UNSET
-    description: Union[Unset, str] = UNSET
-    disconnect_reason: Union[Unset, str] = UNSET
-    sync_time: Union[Unset, float] = UNSET
-    institution: Union[Unset, "ConnectionsInstitutionData"] = UNSET
-    is_mock_data: Union[Unset, bool] = UNSET
-    service_account_ref: Union[Unset, "ServiceAccountRefType"] = UNSET
+    service_name: Union[Unset, BaseConnectionsResponseDataV2ServiceName] = UNSET
+    description: Union[None, Unset, str] = UNSET
+    status: Union[None, Unset, str] = UNSET
+    created_at: Union[None, Unset, datetime.datetime] = UNSET
+    updated_at: Union[None, Unset, datetime.datetime] = UNSET
+    disconnect_reason: Union[None, Unset, str] = UNSET
+    sync_time: Union[None, Unset, float] = UNSET
+    institution: Union["ConnectionsInstitutionData", None, Unset] = UNSET
+    is_mock_data: Union[None, Unset, bool] = UNSET
+    service_account_ref: Union["ServiceAccountRefType", None, Unset] = UNSET
     additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
-        connection_uuid = self.connection_uuid
+        from ..models.connections_institution_data import ConnectionsInstitutionData
+        from ..models.service_account_ref_type import ServiceAccountRefType
 
-        service_name: str = self.service_name
-
-        status = self.status
-
-        created_at: Union[None, str]
-        if isinstance(self.created_at, datetime.datetime):
-            created_at = self.created_at.isoformat()
+        connection_uuid: Union[None, Unset, str]
+        if isinstance(self.connection_uuid, Unset):
+            connection_uuid = UNSET
         else:
-            created_at = self.created_at
-
-        updated_at: Union[None, str]
-        if isinstance(self.updated_at, datetime.datetime):
-            updated_at = self.updated_at.isoformat()
-        else:
-            updated_at = self.updated_at
+            connection_uuid = self.connection_uuid
 
         first_record_date: Union[None, Unset, str]
         if isinstance(self.first_record_date, Unset):
@@ -78,37 +71,89 @@ class BaseConnectionsResponseDataV2:
         else:
             first_record_date = self.first_record_date
 
-        description = self.description
+        service_name: Union[Unset, str] = UNSET
+        if not isinstance(self.service_name, Unset):
+            service_name = self.service_name
 
-        disconnect_reason = self.disconnect_reason
+        description: Union[None, Unset, str]
+        if isinstance(self.description, Unset):
+            description = UNSET
+        else:
+            description = self.description
 
-        sync_time = self.sync_time
+        status: Union[None, Unset, str]
+        if isinstance(self.status, Unset):
+            status = UNSET
+        else:
+            status = self.status
 
-        institution: Union[Unset, Dict[str, Any]] = UNSET
-        if not isinstance(self.institution, Unset):
+        created_at: Union[None, Unset, str]
+        if isinstance(self.created_at, Unset):
+            created_at = UNSET
+        elif isinstance(self.created_at, datetime.datetime):
+            created_at = self.created_at.isoformat()
+        else:
+            created_at = self.created_at
+
+        updated_at: Union[None, Unset, str]
+        if isinstance(self.updated_at, Unset):
+            updated_at = UNSET
+        elif isinstance(self.updated_at, datetime.datetime):
+            updated_at = self.updated_at.isoformat()
+        else:
+            updated_at = self.updated_at
+
+        disconnect_reason: Union[None, Unset, str]
+        if isinstance(self.disconnect_reason, Unset):
+            disconnect_reason = UNSET
+        else:
+            disconnect_reason = self.disconnect_reason
+
+        sync_time: Union[None, Unset, float]
+        if isinstance(self.sync_time, Unset):
+            sync_time = UNSET
+        else:
+            sync_time = self.sync_time
+
+        institution: Union[Dict[str, Any], None, Unset]
+        if isinstance(self.institution, Unset):
+            institution = UNSET
+        elif isinstance(self.institution, ConnectionsInstitutionData):
             institution = self.institution.to_dict()
+        else:
+            institution = self.institution
 
-        is_mock_data = self.is_mock_data
+        is_mock_data: Union[None, Unset, bool]
+        if isinstance(self.is_mock_data, Unset):
+            is_mock_data = UNSET
+        else:
+            is_mock_data = self.is_mock_data
 
-        service_account_ref: Union[Unset, Dict[str, Any]] = UNSET
-        if not isinstance(self.service_account_ref, Unset):
+        service_account_ref: Union[Dict[str, Any], None, Unset]
+        if isinstance(self.service_account_ref, Unset):
+            service_account_ref = UNSET
+        elif isinstance(self.service_account_ref, ServiceAccountRefType):
             service_account_ref = self.service_account_ref.to_dict()
+        else:
+            service_account_ref = self.service_account_ref
 
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
-        field_dict.update(
-            {
-                "connectionUuid": connection_uuid,
-                "serviceName": service_name,
-                "status": status,
-                "createdAt": created_at,
-                "updatedAt": updated_at,
-            }
-        )
+        field_dict.update({})
+        if connection_uuid is not UNSET:
+            field_dict["connectionUuid"] = connection_uuid
         if first_record_date is not UNSET:
             field_dict["firstRecordDate"] = first_record_date
+        if service_name is not UNSET:
+            field_dict["serviceName"] = service_name
         if description is not UNSET:
             field_dict["description"] = description
+        if status is not UNSET:
+            field_dict["status"] = status
+        if created_at is not UNSET:
+            field_dict["createdAt"] = created_at
+        if updated_at is not UNSET:
+            field_dict["updatedAt"] = updated_at
         if disconnect_reason is not UNSET:
             field_dict["disconnectReason"] = disconnect_reason
         if sync_time is not UNSET:
@@ -128,41 +173,15 @@ class BaseConnectionsResponseDataV2:
         from ..models.service_account_ref_type import ServiceAccountRefType
 
         d = src_dict.copy()
-        connection_uuid = d.pop("connectionUuid")
 
-        service_name = check_base_connections_response_data_v2_service_name(d.pop("serviceName"))
-
-        status = d.pop("status")
-
-        def _parse_created_at(data: object) -> Union[None, datetime.datetime]:
+        def _parse_connection_uuid(data: object) -> Union[None, Unset, str]:
             if data is None:
                 return data
-            try:
-                if not isinstance(data, str):
-                    raise TypeError()
-                created_at_type_0 = isoparse(data)
-
-                return created_at_type_0
-            except:  # noqa: E722
-                pass
-            return cast(Union[None, datetime.datetime], data)
-
-        created_at = _parse_created_at(d.pop("createdAt"))
-
-        def _parse_updated_at(data: object) -> Union[None, datetime.datetime]:
-            if data is None:
+            if isinstance(data, Unset):
                 return data
-            try:
-                if not isinstance(data, str):
-                    raise TypeError()
-                updated_at_type_0 = isoparse(data)
+            return cast(Union[None, Unset, str], data)
 
-                return updated_at_type_0
-            except:  # noqa: E722
-                pass
-            return cast(Union[None, datetime.datetime], data)
-
-        updated_at = _parse_updated_at(d.pop("updatedAt"))
+        connection_uuid = _parse_connection_uuid(d.pop("connectionUuid", UNSET))
 
         def _parse_first_record_date(data: object) -> Union[None, Unset, datetime.datetime]:
             if data is None:
@@ -181,36 +200,134 @@ class BaseConnectionsResponseDataV2:
 
         first_record_date = _parse_first_record_date(d.pop("firstRecordDate", UNSET))
 
-        description = d.pop("description", UNSET)
-
-        disconnect_reason = d.pop("disconnectReason", UNSET)
-
-        sync_time = d.pop("syncTime", UNSET)
-
-        _institution = d.pop("institution", UNSET)
-        institution: Union[Unset, ConnectionsInstitutionData]
-        if isinstance(_institution, Unset):
-            institution = UNSET
+        _service_name = d.pop("serviceName", UNSET)
+        service_name: Union[Unset, BaseConnectionsResponseDataV2ServiceName]
+        if isinstance(_service_name, Unset):
+            service_name = UNSET
         else:
-            institution = ConnectionsInstitutionData.from_dict(_institution)
+            service_name = check_base_connections_response_data_v2_service_name(_service_name)
 
-        is_mock_data = d.pop("isMockData", UNSET)
+        def _parse_description(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
 
-        _service_account_ref = d.pop("serviceAccountRef", UNSET)
-        service_account_ref: Union[Unset, ServiceAccountRefType]
-        if isinstance(_service_account_ref, Unset):
-            service_account_ref = UNSET
-        else:
-            service_account_ref = ServiceAccountRefType.from_dict(_service_account_ref)
+        description = _parse_description(d.pop("description", UNSET))
+
+        def _parse_status(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        status = _parse_status(d.pop("status", UNSET))
+
+        def _parse_created_at(data: object) -> Union[None, Unset, datetime.datetime]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, str):
+                    raise TypeError()
+                created_at_type_0 = isoparse(data)
+
+                return created_at_type_0
+            except:  # noqa: E722
+                pass
+            return cast(Union[None, Unset, datetime.datetime], data)
+
+        created_at = _parse_created_at(d.pop("createdAt", UNSET))
+
+        def _parse_updated_at(data: object) -> Union[None, Unset, datetime.datetime]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, str):
+                    raise TypeError()
+                updated_at_type_0 = isoparse(data)
+
+                return updated_at_type_0
+            except:  # noqa: E722
+                pass
+            return cast(Union[None, Unset, datetime.datetime], data)
+
+        updated_at = _parse_updated_at(d.pop("updatedAt", UNSET))
+
+        def _parse_disconnect_reason(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        disconnect_reason = _parse_disconnect_reason(d.pop("disconnectReason", UNSET))
+
+        def _parse_sync_time(data: object) -> Union[None, Unset, float]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, float], data)
+
+        sync_time = _parse_sync_time(d.pop("syncTime", UNSET))
+
+        def _parse_institution(data: object) -> Union["ConnectionsInstitutionData", None, Unset]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, dict):
+                    raise TypeError()
+                institution_type_1 = ConnectionsInstitutionData.from_dict(data)
+
+                return institution_type_1
+            except:  # noqa: E722
+                pass
+            return cast(Union["ConnectionsInstitutionData", None, Unset], data)
+
+        institution = _parse_institution(d.pop("institution", UNSET))
+
+        def _parse_is_mock_data(data: object) -> Union[None, Unset, bool]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, bool], data)
+
+        is_mock_data = _parse_is_mock_data(d.pop("isMockData", UNSET))
+
+        def _parse_service_account_ref(data: object) -> Union["ServiceAccountRefType", None, Unset]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            try:
+                if not isinstance(data, dict):
+                    raise TypeError()
+                service_account_ref_type_1 = ServiceAccountRefType.from_dict(data)
+
+                return service_account_ref_type_1
+            except:  # noqa: E722
+                pass
+            return cast(Union["ServiceAccountRefType", None, Unset], data)
+
+        service_account_ref = _parse_service_account_ref(d.pop("serviceAccountRef", UNSET))
 
         base_connections_response_data_v2 = cls(
             connection_uuid=connection_uuid,
+            first_record_date=first_record_date,
             service_name=service_name,
+            description=description,
             status=status,
             created_at=created_at,
             updated_at=updated_at,
-            first_record_date=first_record_date,
-            description=description,
             disconnect_reason=disconnect_reason,
             sync_time=sync_time,
             institution=institution,
--- railz-api-client/railz_api_client/models/get_expense_response_v2_dto.py
+++ railz-api-client/railz_api_client/models/get_expense_response_v2_dto.py
@@ -1,7 +1,9 @@
-from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar
+from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar, Union
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
 
 if TYPE_CHECKING:
     from ..models.basic_start_end_date_report_meta_data_v2 import BasicStartEndDateReportMetaDataV2
@@ -17,34 +19,37 @@ class GetExpenseResponseV2Dto:
     """
     Attributes:
         pagination (PaginationMetaData):
-        meta (BasicStartEndDateReportMetaDataV2):
         data (List['GetExpenseData']):
+        meta (Union[Unset, BasicStartEndDateReportMetaDataV2]):
     """
 
     pagination: "PaginationMetaData"
-    meta: "BasicStartEndDateReportMetaDataV2"
     data: List["GetExpenseData"]
+    meta: Union[Unset, "BasicStartEndDateReportMetaDataV2"] = UNSET
     additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
         pagination = self.pagination.to_dict()
-
-        meta = self.meta.to_dict()
 
         data = []
         for data_item_data in self.data:
             data_item = data_item_data.to_dict()
             data.append(data_item)
 
+        meta: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.meta, Unset):
+            meta = self.meta.to_dict()
+
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update(
             {
                 "pagination": pagination,
-                "meta": meta,
                 "data": data,
             }
         )
+        if meta is not UNSET:
+            field_dict["meta"] = meta
 
         return field_dict
 
@@ -57,8 +62,6 @@ class GetExpenseResponseV2Dto:
         d = src_dict.copy()
         pagination = PaginationMetaData.from_dict(d.pop("pagination"))
 
-        meta = BasicStartEndDateReportMetaDataV2.from_dict(d.pop("meta"))
-
         data = []
         _data = d.pop("data")
         for data_item_data in _data:
@@ -66,10 +69,17 @@ class GetExpenseResponseV2Dto:
 
             data.append(data_item)
 
+        _meta = d.pop("meta", UNSET)
+        meta: Union[Unset, BasicStartEndDateReportMetaDataV2]
+        if isinstance(_meta, Unset):
+            meta = UNSET
+        else:
+            meta = BasicStartEndDateReportMetaDataV2.from_dict(_meta)
+
         get_expense_response_v2_dto = cls(
             pagination=pagination,
-            meta=meta,
             data=data,
+            meta=meta,
         )
 
         get_expense_response_v2_dto.additional_properties = d
